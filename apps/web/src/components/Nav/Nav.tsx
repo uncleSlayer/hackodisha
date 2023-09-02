8a83aef3213070818c39318ad54509e835166639
samyak swaroop mishra <samyak2k16@gmail.com>
2023-09-03 02:17:07 +0530

    nav sam

--- apps/web/src/components/Nav/Nav.tsx
+++ apps/web/src/components/Nav/Nav.tsx
@@ -8,17 +8,25 @@ import Carousel from '../../assets/carousel.png'
 
 const Nav = () => {
     return (
-        <div className="nav_container flex">
-            <div className="logo_container flex ">
-                <h1 className="logotxt">H</h1>
-                <img className="logo" src={Logo} alt="dd" />
+        <div className="flexC sticky">
+            <div className="nav_container flex">
+                <div className="logo_container flex ">
+                    <h1 className="logotxt">H</h1>
+                    <img className="logo" src={Logo} alt="dd" />
+                    <h1 className="logotxt">.</h1>
+                </div>
+                
+                <div className="navopt_container flex">
+                    <div className="navopt flex"><img className="navopt_img" src={Carousel} alt="dd" /></div>
+                    <div className="navopt flex"><img className="navopt_img" src={Cart} alt="dd" /></div>
+                    <div className="navopt flex"><img className="navopt_img" src={User} alt="dd" /></div>
+                </div>
             </div>
-            <div className="navopt_container flex">
-                <div className="navopt flex"><img className="navopt_img" src={Carousel} alt="dd" /></div>
-                <div className="navopt flex"><img className="navopt_img" src={Cart} alt="dd" /></div>
-                <div className="navopt flex"><img className="navopt_img" src={User} alt="dd" /></div>
-            </div>
+            <div className="dividerthin"> </div>
+            
+            
         </div>
+        
     )
 }
 
