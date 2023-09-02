ee078d9c17b344731d2eb360d948f5648b039b06
samyak swaroop mishra <samyak2k16@gmail.com>
2023-09-03 01:35:47 +0530

    last commit to branch

--- apps/web/src/components/Nav/Nav.tsx
+++ apps/web/src/components/Nav/Nav.tsx
@@ -2,7 +2,9 @@ import '../../index.css'
 import './nav.css'
 
 import Logo from '../../assets/pitcher.png'
-
+import Cart from '../../assets/cart.png'
+import User from '../../assets/user.png'
+import Carousel from '../../assets/carousel.png'
 
 const Nav = () => {
     return (
@@ -12,9 +14,9 @@ const Nav = () => {
                 <img className="logo" src={Logo} alt="dd" />
             </div>
             <div className="navopt_container flex">
-                <div className="navopt flex">B</div>
-                <div className="navopt flex">C</div>
-                <div className="navopt flex">U</div>
+                <div className="navopt flex"><img className="navopt_img" src={Carousel} alt="dd" /></div>
+                <div className="navopt flex"><img className="navopt_img" src={Cart} alt="dd" /></div>
+                <div className="navopt flex"><img className="navopt_img" src={User} alt="dd" /></div>
             </div>
         </div>
     )
