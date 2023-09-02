0f34964e80de623e874cf6131e9007028dd1b55b
samyak swaroop mishra <samyak2k16@gmail.com>
2023-09-02 23:17:07 +0530

    nav changes

--- apps/web/src/components/Nav/Nav.tsx
+++ apps/web/src/components/Nav/Nav.tsx
@@ -1,13 +1,15 @@
 import '../../index.css'
 import './nav.css'
 
+import Logo from '../../assets/pitcher.png'
+
 
 const Nav = () => {
     return (
         <div className="nav_container flex">
             <div className="logo_container flex ">
                 <h1 className="logotxt">H</h1>
-                <img className="logo" src="../../assets/pitcher.png" alt="dd" />
+                <img className="logo" src={Logo} alt="dd" />
             </div>
             <div className="navopt_container flex">
                 <div className="navopt flex">B</div>
