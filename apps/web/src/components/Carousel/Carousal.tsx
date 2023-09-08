775ee52a1d0e47d30dd32cb34098022ca48b980f
Gaurav07076 <103797867+Gaurav07076@users.noreply.github.com>
2023-09-09 01:00:09 +0530

    cartfetch to carousel

--- apps/web/src/components/Carousel/Carousal.tsx
+++ apps/web/src/components/Carousel/Carousal.tsx
@@ -3,10 +3,23 @@ import React from 'react'
 import '../../index.css'
 import './carousel.css'
 
+interface Carousel{
+  data: { id: number,
+    name: string,
+    category: string,
+    price: number,
+    description: string,
+    quantity: number,
+    imageUrl: string,
+    userId: number,
+  }[]
+   
+}
 
-const Carousal = () => {
+const Carousal:React.FC<Carousel> = ({data}) => {
+  console.log(data)
   return (
-    <div className="carousel_container">Carofdhtduusal</div>
+    <div className="carousel_container">{}</div>
   )
 }
 
