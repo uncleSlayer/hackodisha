4e4080a785c3da2a37fd96a894586b3260c5ce15
Gaurav07076 <103797867+Gaurav07076@users.noreply.github.com>
2023-09-10 18:22:58 +0530

    seefds

--- apps/web/src/components/Hero/Hero.tsx
+++ apps/web/src/components/Hero/Hero.tsx
@@ -16,9 +16,16 @@ const Hero = () => {
         <div id="panel2" className="section2">
           <div className="flex banertxt_container">
               <div className="banertxt_subcontainer">
-                  <div className="header2">Morem tsum dolor sit?</div>
-                  <div className="subtext2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam quis dicta velit doloremque perspiciatis illo fuga ipsum ut nemo! Quae perferendis dolorum eius nisi natus error blanditiis aut nam dolore eius nisi natus error blanditiis aut nam!mque perspiciatis illo fuga ipsum ut nemo! Qulor sit amet consectetur adipisicing
-                  amet consectetur adipisicin.
+                  <div className="header2">What is Local Lane?</div>
+                  <div className="subtext2">"Local Lane" is a purpose-driven e-commerce website that empowers local traders and safeguards the cultural heritage of India. Our platform offers a user-friendly solution for vendors to showcase and sell their products online, and for users to discover and purchase these unique items. Here are some key features and solutions:
+<ul >
+<li className='hero-list'>Vendor Empowerment</li>
+
+<li className='hero-list'>User-Friendly Shopping</li>
+
+<li className='hero-list'>Safeguarding Heritage</li>
+</ul>
+
                   </div>
               </div>
               <div className="dividerthin_V"></div>
